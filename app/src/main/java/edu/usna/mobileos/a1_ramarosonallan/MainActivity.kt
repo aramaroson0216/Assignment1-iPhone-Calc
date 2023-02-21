a596f851ea5dc63c9c778556e86bfcaa703cd416
Allan T Ramaroson <ahitoramaroson@gmail.com>
2023-02-21 15:07:42 -0500

    Add files via upload

---
+++ app/src/main/java/edu/usna/mobileos/a1_ramarosonallan/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.usna.mobileos.a1_ramarosonallan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
